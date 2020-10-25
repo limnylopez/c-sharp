fd5f004416cea68f628d871c3222d24a73c087f2
Limny Lopez <limny.lopez10@gmail.com>
2020-10-24 22:28:06 -0600

    Funciones

---
+++ Ejercicios/6-Funciones/Program.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno(1, "Limny", "Lopez");
+            Alumno b = new Alumno(2, "Maria", "Martinez");
+
+            a.inactivarAlumno();
+
+            Console.WriteLine(a.nombreCompleto());
+            Console.WriteLine(b.nombreCompleto());
+
+            a.activarAlumno();
+
+            Console.WriteLine("");
+
+            Console.WriteLine(a.nombreCompleto());
+            Console.WriteLine(b.nombreCompleto());
+        }
+    }
+}
